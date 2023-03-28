1f364650dabd3f092ff7b53d95608ff2fdb0f517
Hasan Baig <baig.hasan@outlook.com>
2023-03-28 15:24:13 -0400

    Fixed indenting error in printMenu()

--- CustomerSystem.py
+++ CustomerSystem.py
@@ -7,14 +7,14 @@ import csv
 
 def printMenu():
     print('''
-          Customer and Sales System\n
-          1. Enter Customer Information\n
-          2. Generate Customer data file\n
-          3. Report on total Sales (Not done in this part)\n
-          4. Check for fraud in sales data (Not done in this part)\n
-          9. Quit\n
-          Enter menu option (1-9)
-          ''')
+            Customer and Sales System\n
+            1. Enter Customer Information\n
+            2. Generate Customer data file\n
+            3. Report on total Sales (Not done in this part)\n
+            4. Check for fraud in sales data (Not done in this part)\n
+            9. Quit\n
+            Enter menu option (1-9)
+        ''')
 
 '''
     This function is to be edited to achieve the task.
